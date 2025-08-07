451d952498f2d0e39928ce5018f589c8874f9e1e
Misael C. Homem <misael.homem@gmail.com>
2025-08-07 20:02:55 -0300

    fix: removing comments.

--- IBGE.CNAE.Api/Program.cs
+++ IBGE.CNAE.Api/Program.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICnaeService, CnaeService>();
@@ -19,7 +16,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/cnae", async ([FromServices] ICnaeService cnaeService, [FromQuery] int pageNumber = 1, [FromQuery] int size = 10) =>
+app.MapGet("/api/cnae", async (
+    [FromServices] ICnaeService cnaeService,
+    [FromQuery] int pageNumber = 1,
+    [FromQuery] int size = 10) =>
 {
     var list = await cnaeService.GetAllAsync(pageNumber, size);
 
